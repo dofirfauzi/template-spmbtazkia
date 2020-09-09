ae6f3e2f1ff3a7f993dcadbe488812f8c578750f
Dofir Fauzi <dofirfauzi.1302@gmail.com>
2020-09-09 16:28:18 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.main-slider').owlCarousel({
+    loop: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    animateOut: 'fadeOut',
+    autoplayHoverPause: true,
+    items: 1,
+})
